b420968f27b731955d283a7507daa1e75e0947f7
sammyoto <samgfales@gmail.com>
2026-01-22 21:43:51 -0700

    added dartman, changing to using scenes for simplicity

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,12 @@ use bevy::input::mouse::AccumulatedMouseMotion;
 
 // Resource for storing Mesh Handles
 #[derive(Resource, Default)]
-struct MeshHandles(HashMap<String, Handle<Mesh>>);
-
-// Resource for storing Material Handles
-#[derive(Resource, Default)]
-struct MaterialHandles(HashMap<String, Handle<StandardMaterial>>);
+struct SceneHandles(HashMap<String, Handle<Scene>>);
 
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
-    .init_resource::<MeshHandles>()
-    .init_resource::<MaterialHandles>()
+    .init_resource::<SceneHandles>()
     .add_systems(Startup, (startup, load_assets, spawn_player.after(load_assets)))
     .add_systems(Update, (update_camera))
     .run();
@@ -35,52 +30,22 @@ fn startup(
     ));
 }
 
-fn spawn_player(
+fn spawn_player( 
     mut commands: Commands,
-    mesh_handles: Res<MeshHandles>,
     assets: Res<AssetServer>,
-    material_handles: Res<MaterialHandles>
 ) {
-    let scene: Handle<Scene> = assets.load("models/goblin.glb#Scene0");
-    commands.spawn(SceneRoot(scene));
+    let player: Handle<Scene> = assets.load("models/dartman.glb#Scene0");
+    commands.spawn(SceneRoot(player));
 }
 
 fn load_assets(
-    asset_server: Res<AssetServer>,
-    mut mesh_handles: ResMut<MeshHandles>,
-    mut material_handles: ResMut<MaterialHandles>,
-    mut materials: ResMut<Assets<StandardMaterial>>
+    assets: Res<AssetServer>,
+    mut scene_handles: ResMut<SceneHandles>,
 ) {
-    let player_handle: Handle<Mesh> = asset_server.load(
-        GltfAssetLabel::Primitive {
-            mesh: 0,
-            primitive: 0
-        }
-        .from_asset("models/lowpoly_basic.glb"),
-    );
-
-    let dart_handle: Handle<Mesh> = asset_server.load(
-        GltfAssetLabel::Primitive {
-            mesh: 0,
-            primitive: 0,
-        }
-        .from_asset("models/dart.glb"),
-    );
-
-    mesh_handles.0.insert("Player".to_string(), player_handle);
-    mesh_handles.0.insert("Dart".to_string(), dart_handle);
-
-    let player_material_handle = materials.add(StandardMaterial {
-        base_color: Color::srgb(0.8, 0.7, 0.6),
-        ..default()
-    });
-    let dart_material_handle = materials.add(StandardMaterial {
-        base_color: Color::srgb(0.4, 0.7, 0.8),
-        ..default()
-    });
-
-    material_handles.0.insert("Player".to_string(), player_material_handle);
-    material_handles.0.insert("Dart".to_string(), dart_material_handle);
+   let goblin: Handle<Scene> = assets.load("models/goblin.glb#Scene0");
+   scene_handles.0.insert("Goblin".to_string(), goblin);
+   let dart: Handle<Scene> = assets.load("models/dart.glb#Scene0");
+   scene_handles.0.insert("Dart".to_string(), dart);
 }
 
 fn update_camera(
