31cab086ca650ce6256f6b550171fbb96af198d2
sammyoto <samgfales@gmail.com>
2026-01-28 18:57:53 -0700

    added goblins, made them chase player

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,20 @@ use bevy::{prelude::*,
     };
 use std::collections::HashMap;
 use bevy::prelude::ops::sqrt;
+use rand::prelude::*;
 
 // Resource for storing Mesh Handles
 #[derive(Resource, Default)]
 struct SceneHandles(HashMap<String, Handle<Scene>>);
 
+#[derive(Resource, Default)]
+struct SpawnTimer(f32);
+
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Goblin;
 
 #[derive(Component)]
 struct Dart;
@@ -49,15 +56,19 @@ fn main() {
             //FrameTimeDiagnosticsPlugin::default(),
         ))
     .init_resource::<SceneHandles>()
+    .init_resource::<SpawnTimer>()
     .add_systems(Startup, (startup, load_assets, spawn_player, spawn_camera).chain())
-    .add_systems(Update, (player_look, move_player, move_camera, throw_dart, update_darts).chain())
+    .add_systems(Update, (player_look, move_player, move_camera, spawn_goblins, update_goblins, throw_dart, update_darts).chain())
     .run();
 }
 
 fn startup(
     mut commands: Commands,
+    mut spawn_timer: ResMut<SpawnTimer>,
     assets: Res<AssetServer>,
 ) {
+    // Init Spawn Timer
+    spawn_timer.0 = 0.0;
     // Arena
     let arena: Handle<Scene> = assets.load("models/dartman_arena.glb#Scene0");
     commands.spawn((
@@ -69,7 +80,7 @@ fn startup(
     commands.spawn(
         (DirectionalLight
         {  
-            illuminance: 10000.0,
+            illuminance: 9000.0,
             color: Color::WHITE,
             shadows_enabled: true,
             ..default()
@@ -196,5 +207,64 @@ fn update_darts(
         let dart_forward = dart.forward().as_vec3();
         let dart_speed: f32 = 20.0;
         dart.translation +=  dart_forward * dart_speed * time.delta_secs();
+    }
+}
+
+fn spawn_goblins(
+    mut commands: Commands,
+    scene_handles: Res<SceneHandles>,
+    mut spawn_timer: ResMut<SpawnTimer>,
+    time: Res<Time>
+) {
+    // Spawn goblins every 3 seconds
+    if time.elapsed_secs() - spawn_timer.0 <= 3.0 {
+        return;
+    }
+
+    let mut rng = rand::thread_rng();
+    let spawn_number = rng.gen_range(1..10);
+    let sides = ["TOP", "BOTTOM", "LEFT", "RIGHT"];
+
+    for _ in 0..spawn_number {
+        let side = sides[rng.gen_range(0..sides.len())];
+        let mut x: f32 = 0.0;
+        let mut z: f32 = 0.0;
+
+        if side == "TOP" {
+            x = rng.gen_range(-90.0..90.0);
+            z = -90.0
+        } else if side == "BOTTOM" {
+            x = rng.gen_range(-90.0..90.0);
+            z = 90.0
+        } else if side == "LEFT" {
+            x = -90.0;
+            z = rng.gen_range(-90.0..90.0);
+        } else if side == "RIGHT" {
+            x = 90.0;
+            z = rng.gen_range(-90.0..90.0);
+        }
+
+        commands.spawn((
+            SceneRoot(scene_handles.0.get("Goblin").unwrap().clone()),
+            Transform::from_xyz(x, 0.0, z),
+            Goblin
+        ));
+    }
+
+    spawn_timer.0 = time.elapsed_secs();
+}
+
+fn update_goblins(
+    mut goblins: Query<&mut Transform, (With<Goblin>, Without<Player>)>,
+    player: Single<&Transform, With<Player>>,
+    time: Res<Time>
+) {
+    for mut goblin in goblins.iter_mut() {
+        // Look at player
+        goblin.look_at(player.translation, Vec3::Y);
+        // Move towards player
+        let forward = goblin.forward().as_vec3();
+        let speed: f32 = 10.0;
+        goblin.translation +=  forward * speed * time.delta_secs();
     }
 }
